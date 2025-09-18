a84f9e53c3856a86b0a5b833cb25d76d90658bec
Nach015 <nachozuv@outlook.com>
2025-09-18 20:11:32 -0300

    Tp listas

---
+++ tp listas.py
@@ -0,0 +1,67 @@
+#Punto1
+nums= list(range(4, 101, 4))
+print(nums)
+
+#Punto2
+lista=[1,2,3,4,5]
+print(lista[4])
+
+#Punto3
+lista_vacia=[]
+lista_vacia.append("Hola,Chau,Algo")
+
+print(lista_vacia)
+
+#Punto4
+
+animales= ["perro", "gato", "conejo", "pez"]
+animales[1] = "loro"
+animales[-1] = "oso"
+print(animales)
+
+#Punto5
+
+#el programa elimina el valor máximo de la lista (22) y luego imprime la lista sin ese valor.
+
+#Punto6
+
+num= list((range(10,31,5)))
+
+print(num)
+
+#Punto7
+
+autos = ["sedan", "polo", "suran", "gol"]
+autos[1]= "Onyx"
+autos[2]= "Focus"
+print(autos)
+
+#Punto8
+
+dobles=[]
+dobles.append(5 * 2)
+dobles.append(10 * 2)
+dobles.append(15 * 2)
+print(dobles)
+
+#Punto9
+
+compras = [["pan", "leche"], ["arroz", "fideos", "salsa"], ["agua"]]
+compras[2].append("jugo")
+compras[1][1]="tallarines"
+compras[0].remove("pan")
+print(compras)
+
+#Punto10
+
+lista_anidada = [
+    15,                  
+    True,                
+    [25.5, 57.9, 30.6], 
+    False                
+]
+
+
+print(lista_anidada)
+
+
